6f6a5ed1490cf40687c516dffde5f3b1461ba5e0
leonado10000 <rjdispair@gmail.com>
2023-02-01 20:13:35 +0530

    Additional css and alien x
    Added css for button
    -newer arrangement
    -alien x for reference

--- Ben10/alien/views.py
+++ Ben10/alien/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-
+k = [ "https://static.wikia.nocookie.net/ben10/images/3/38/OV_AlienX.png/revision/latest?cb=20200518024327" ]
+cont = ["Alien X"]
 def index(request):
     try:
-        return render(request, "alien/index.html")
+        return render(request, "alien/index.html" , {
+            "aliens":k,"cont":cont
+        })
     except:
         return HttpResponse("heello world")
