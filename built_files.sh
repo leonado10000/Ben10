8a87b2a7169c145dcb9f50ec15242ca383e8174e
leonado10000 <rjdispair@gmail.com>
2023-02-12 12:06:23 +0530

    update on name and staticfiles_built
    changed naming error
    - .text to .txt
    - python3.p to 3.9
    
    changed setting

--- built_files.sh
+++ built_files.sh
@@ -1,4 +1,4 @@
 echo "built start"
-python3.9 -m pip install -r requirements.text
-python3.p manage.py collectstatic --noinput --clear
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
 echo "End built"
